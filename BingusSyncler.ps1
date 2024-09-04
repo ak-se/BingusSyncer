de2f8d078a34f2a24ddc2afdc360344ddfbfd3d3
Akshat@SE <160997035+ak-se@users.noreply.github.com>
2024-09-04 16:25:00 +0530

    Update BingusSyncler.ps1
    added comments to script

--- BingusSyncler.ps1
+++ BingusSyncler.ps1
@@ -1,15 +1,16 @@
-﻿Write-Host "Initiating BingusSyncer"
+#init
+Write-Host "Initiating BingusSyncer"
+#sleep for goodluck
 Start-Sleep -Milliseconds 250
 # Checking if Z is mounted
-if (Get-PSDrive -Name Z -ErrorAction SilentlyContinue) {
-    Write-Host "Z found, nooot noot, inishiyating synk"
+if (Get-PSDrive -Name Z -ErrorAction SilentlyContinue) 
+    #if Z mounted, start copying
+    { Write-Host "Z found, nooot noot, inishiyating synk" & "Robocopy.exe" Z:\ C:\bingus\ /MIR /FFT /Z /XA:H /W:5 } 
 
-    & "Robocopy.exe" Z:\ C:\bingus\ /MIR /FFT /Z /XA:H /W:5
+        # if z not mounted, throw error
+        
+        
+else { Write-Host "Z not found :'( womp womp" -ForegroundColor Red }
 
-} else {
-
-    Write-Host "Z not found :'( womp womp" -ForegroundColor Red
-
-}
-
+# End Credits
 Write-Host "BingusSyncer will exit now"
