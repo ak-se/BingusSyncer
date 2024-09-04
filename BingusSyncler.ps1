33a76a41d4056c4e9f8e8f3eda2d9f972085a6fc
ak-se <ak-se@tutamail.com>
2024-09-04 16:06:29 +0530

    Init, script v1, make readme

---
+++ BingusSyncler.ps1
@@ -0,0 +1,15 @@
+﻿Write-Host "Initiating BingusSyncer"
+Start-Sleep -Milliseconds 250
+# Checking if Z is mounted
+if (Get-PSDrive -Name Z -ErrorAction SilentlyContinue) {
+    Write-Host "Z found, nooot noot, inishiyating synk"
+
+    & "Robocopy.exe" Z:\ C:\bingus\ /MIR /FFT /Z /XA:H /W:5
+
+} else {
+
+    Write-Host "Z not found :'( womp womp" -ForegroundColor Red
+
+}
+
+Write-Host "BingusSyncer will exit now"
---
+++ README.md
@@ -0,0 +1,5 @@
+# BingusSyncer
+
+## Simple Powershell script that uses Robocopy to Mirror a shared directory between the Test Windows VM and Host System
+
+
